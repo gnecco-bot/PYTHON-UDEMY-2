506b9edd9affc0257f53343dfea2b1a5c67ca268
Lucas <lucasalvesgnecco@gmail.com>
2026-08-30 16:24:39 -0300

    Exercicie IF

---
+++ Treino de Conhecimentos (Exercicios) S4/1_IF.py
@@ -0,0 +1,42 @@
+# 1. Large number of the two input sended
+# numOne = input('Put one number: ')
+# numTwo = input('Put a second number: ')
+# 
+# if numOne.isdigit() & numTwo.isdigit():
+#     if int(numOne) > int(numTwo):
+#         print(numOne)
+#     elif int(numOne) < int(numTwo):
+#         print(numTwo)
+#     else:
+#         print('The numbers is equal.')
+# else:
+#     print('Only send numbers.')
+
+# 2. Check a number even or odd (par ou impar)
+# numEvenOrOdd = int(input('Send a number: '))
+# 
+# if numEvenOrOdd % 2 == 0: 
+#     print(f'This number "{numEvenOrOdd}" is Even!')
+#     print(f'This number "{numEvenOrOdd}" sended is Odd.')
+
+# 3. Check the temperature
+# tempUser = int(input('Set your temperature local: '))
+# 
+# if tempUser < 10:
+#     print('It is so cold!') 
+# elif 10 <= tempUser <= 25:
+#     print('It is great!')
+# elif tempUser > 25:
+#     print('It is so hot!')
+# else:
+#     print('Something is wrong :(')
+
+# 4. Calculate income tax
+salaryUser = float(int(input('Put your balance mensaly: ')))
+
+if salaryUser > 5000.00:
+    newBalance = salaryUser * 0.8
+    deducted = salaryUser - newBalance
+    print(f'Your new balance after de income tax is "${newBalance:.2f}" and "${deducted:.2f}" was deducted.')
+else:
+    print('Your salary is less than $5000 and you dont need to pay income tax.')
