6c83b72e5873c0673965cb3ef1ef79bc8f136620
Lucas <lucasalvesgnecco@gmail.com>
2026-08-31 20:13:47 -0300

    Exercicie IF

--- Treino de Conhecimentos (Exercicios) S4/1_IF.py
+++ Treino de Conhecimentos (Exercicios) S4/1_IF.py
@@ -51,16 +51,33 @@
 #     print(f'The word "{word}" is not palindrome.')
 
 # 6. Check de larger of the 3 numbers
+# 
+# num1 = int(input('Send number one: '))
+# num2 = int(input('Send a number two: '))
+# num3 = int(input('Send a number three: '))
+# 
+# if num1 >= num2 and num1 >= num3:
+#     print(f'Number 1 "{num1}" is the larger!')
+# elif num2 >= num1 and num2 >= num3:
+#     print(f'Number 2 "{num2}" is the larger!')
+# elif num3 >=num1 and num3 >= num2:
+#     print(f'Number 3 "{num3}" is the larger!')
+# else:
+#     print('The numers are the same')
 
-num1 = int(input('Send number one: '))
-num2 = int(input('Send a number two: '))
-num3 = int(input('Send a number three: '))
+# 7. Age category
 
-if num1 >= num2 and num1 >= num3:
-    print(f'Number 1 "{num1}" is the larger!')
-elif num2 >= num1 and num2 >= num3:
-    print(f'Number 2 "{num2}" is the larger!')
-elif num3 >=num1 and num3 >= num2:
-    print(f'Number 3 "{num3}" is the larger!')
-else:
-    print('The numers are the same')
+try:
+    age = int(input('Send your age: '))
+    if age > 0 and age <= 12:
+        print('You are a kid!')
+    elif age >= 13 and age <= 17:
+        print('You are a adolescent!')
+    elif age >= 18 and age <= 64:
+        print('You are a adult!')
+    elif age >= 65:
+        print('You are a old human!')
+    else: 
+        print('Value invalid.')
+except ValueError:
+    print('Value invalid, only numbers.')
