90d73552d569a5309a98afe7422140256c48e261
Lucas <lucasalvesgnecco@gmail.com>
2026-09-02 20:43:35 -0300

    Exercicie "FOR"

--- Treino de Conhecimentos (Exercicios) S4/2_FOR.py
+++ Treino de Conhecimentos (Exercicios) S4/2_FOR.py
@@ -10,7 +10,7 @@
 # sum = 0
 # for i in range(0, 101):
 #     sum += i
-#     print(sum)
+#     print(sum)        
 
 # 4. Print the elements of a list
 # list = [5, 10, 15, 20, 25]
@@ -23,3 +23,21 @@
 # for i in word:
 #     letters += 1
 # print(letters)
+
+# 6. Multiplication each number by 3
+# list = [1, 2, 3, 4, 5]
+# for i in list:
+#     print(i*3)
+
+# 7. Print each letters by "python"
+# python = 'python'
+# for i in python:
+#     print(i)
+
+# 8. Count the number of pairs in a list 
+list = [2, 5, 6, 7, 8, 10, 12]
+count = 0
+for i in list:
+    if i % 2 == 0:
+        count += 1
+        print(f'Number of pairs "{i}" in list count: {count}')
