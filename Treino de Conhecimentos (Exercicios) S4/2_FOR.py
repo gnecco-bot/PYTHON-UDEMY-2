2306d54a3f8894a078bfdd857c331b8b4e906963
Lucas <lucasalvesgnecco@gmail.com>
2026-09-01 21:56:41 -0300

    Exercicie "FOR"

--- Treino de Conhecimentos (Exercicios) S4/2_FOR.py
+++ Treino de Conhecimentos (Exercicios) S4/2_FOR.py
@@ -4,6 +4,28 @@
 #     print(i)
 
 # 2. Print the pairs from 1 to 20
+# 
+# for i in range(2,21,2):
+#     print(i)
 
-for i in range(2,21,2):
-    print(i)
+# 3. Sum numbers from 1 to 100
+# 
+# sum = 0
+# for i in range(0, 101):
+#     sum += i
+#     print(sum)
+
+# 4. Print the elements of a list
+# 
+# list = [5, 10, 15, 20, 25]
+# 
+# for i in list:
+#     print(i)
+
+# 5. Count the letters of a word
+word = str(input('Digit a word: '))
+letters = 0
+for i in word:
+    letters += 1
+
+print(letters)
