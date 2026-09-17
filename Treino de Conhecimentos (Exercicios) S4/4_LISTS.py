825dfee87151ef0d9aaba0a501e89c55a5476654
Lucas <lucasalvesgnecco@gmail.com>
2026-09-17 20:31:25 -0300

    Exercicie "LIST"

--- Treino de Conhecimentos (Exercicios) S4/4_LISTS.py
+++ Treino de Conhecimentos (Exercicios) S4/4_LISTS.py
@@ -31,7 +31,22 @@
 # wordInverted = word[::-1]
 # print('List with the word reversed:', wordInverted)
 
-# 7. Given a list of numbers, create a new list that contains only the number larger than 10
-num = [1, 23, 56, 21, 8, 6, 10, 9]
-numLarger = [n for n in num if n > 10]
-print(numLarger)
+# 7. Given a list of numbers, create a new list that contains only the number larger }}than 10
+# num = [1, 23, 56, 21, 8, 6, 10, 9]
+# numLarger = [n for n in num if n > 10]
+# print(numLarger)
+
+# 8. Check if a list contains a value specific
+# num = [1, 23, 56, 21, 8, 6, 10, 9]
+# exists = int(input('Digit a value to check if it contains in a list specific: '))
+# if exists in num:
+#     print(f'This number "{exists}" exists in this list')
+# else:
+#     print('It do not exists in this list.')
+
+# 9. Combine two lists of the numbers, remove duplicates and show it in a list 
+list1 = [1,2,3,4,5]
+list2 = [4,5,6,7,8]
+
+list_combine = list(set(list1 + list2))
+print(list_combine)
