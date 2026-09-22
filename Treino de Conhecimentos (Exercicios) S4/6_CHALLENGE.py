26604de36bfcae445679d443ef980e041d19d8af
Lucas <lucasalvesgnecco@gmail.com>
2026-09-22 20:21:27 -0300

    Challenge 6

--- Treino de Conhecimentos (Exercicios) S4/6_CHALLENGE.py
+++ Treino de Conhecimentos (Exercicios) S4/6_CHALLENGE.py
@@ -25,3 +25,48 @@
 #     else:
 #         return 'Invalid number'
 # print(dayWeek(2))
+
+# 3. Write a program that asks the user for a grade between zero and ten
+# def zeroTen():
+#     while True:
+#         try:
+#             n = float(input('Digit a value between 1 and 10: '))
+#             if 0 <= n <= 10:
+#                 return n
+#             else:
+#                 print('Incorrect value, only 1 to 10')
+#         except ValueError:
+#             print('Incorrect value')
+
+# grade = zeroTen()
+# print(f'You entered the correct note: {grade}')
+
+# 4. Write a program that reads an indeterminate number of numbers
+# def indeterminateNumber():
+#     one_0_25 = 0
+#     one_26_50 = 0
+#     one_51_75 = 0
+#     one_76_100 = 0
+#     while True:
+#         try:
+#             n = int(input('Digit a number between 0 to 100: '))
+#             if 0 <= n <= 25:
+#                 one_0_25+=1
+#             if 26 <= n <= 50:
+#                 one_26_50+=1
+#             if 51 <= n <= 75:
+#                 one_51_75+=1
+#             if 76 <= n <= 100:
+#                 one_76_100+=1
+#             if n < 0:
+#                 print(f'0-25: {one_0_25}')
+#                 print(f'26-50: {one_26_50}')
+#                 print(f'51-75: {one_51_75}')
+#                 print(f'76-100: {one_76_100}')
+#                 return
+#             else:
+#                 pass
+#         except ValueError:
+#             print('Something sended is incorrect, pls check it.') 
+# indeterminateNumber()
+
