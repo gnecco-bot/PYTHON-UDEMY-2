7612a83649f58e0670b672e80b902a356f1dc305
Lucas <lucasalvesgnecco@gmail.com>
2026-09-22 19:56:56 -0300

    Challenge 6

--- Treino de Conhecimentos (Exercicios) S4/6_CHALLENGE.py
+++ Treino de Conhecimentos (Exercicios) S4/6_CHALLENGE.py
@@ -1,10 +1,27 @@
 # 1. Write a program that checks whether a character is a vowel or a consonant
-def vowelConsoant(character):
-    if character.isalpha() and len(character) == 1:
-        if character.upper() in 'AEIOU':
-            return print('is vowel')
-        if character.upper() in 'BCDFGHJKLMNPQRSTVWXYZ':
-            return print('is consonant')
-    else:
-        print('Something sendend isnt one character')
-vowelConsoant('bb')
+# def vowelConsoant(character):
+#     if character.isalpha() and len(character) == 1:
+#         if character.upper() in 'AEIOU':
+#             return print('is vowel')
+#         if character.upper() in 'BCDFGHJKLMNPQRSTVWXYZ':
+#             return print('is consonant')
+#     else:
+#         print('Something sendend isnt one character')
+# vowelConsoant('bb')
+
+# 2. Write a program that reads a number and displays it as the corresponding day of the week.
+# def dayWeek(d):
+#     days = {
+#         1:'Sunday',
+#         2:'Monday',
+#         3:'Tuesday',
+#         4:'Wednesday',
+#         5:'Thursday',
+#         6:'Friday',
+#         7:'Saturday'
+#     }
+#     if d in days:
+#         return f'The day corresponding the number {d} is {days[d]}'
+#     else:
+#         return 'Invalid number'
+# print(dayWeek(2))
